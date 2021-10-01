8867875314fb245ad1730a25bfd8db51e18cac4f
Tomas_Borrego <tomasborrego17@tecnico.ulisboa.pt>
2021-10-01 22:02:37 +0100

    Refinamento da funcao para ler do ficheiro para variaveis estaticas

--- proj_int.c
+++ proj_int.c
@@ -8,16 +8,53 @@ void main () {
     //char tab_c[2] = {"0"}, tab_l[2] = {"0"}, tes_l[2] = {"0"}, tes_c[2] = {"0"}, tipo_int[2] = {"0"}, num_pretas[3] = {"0"};
     int tab_c = 0, tab_l = 0, tes_l = 0, tes_c = 0, num_pretas = 0;
     char tipo_int[2] = {"0"};
-    int parede = 0;
+    int parede_1 = 0, parede_2 = 0, parede_v = 0;
 
     char ch = '0', ch_next = '0';
     char temp_string[4] = {"0"};
     int cont = 0;
     int cont_n = 0, t = 0;
 
+    int t_counter = 0;
+
 
 
     fp = fopen("enunciado202.in1", "r");
+
+    // Calcula a quantidade de informação da tabela + paredes
+
+    for (int i = 0; i < 100; i++) {
+        fscanf(fp, " ");
+        fscanf(fp, "\n");
+
+        if(feof(fp) != 0) break; 
+
+        if (i > 5 /* && tipo_jogo != A6 */) {    // Ler Paredes para apenas 1 int
+
+            if (t_counter == 0) {
+                fscanf(fp, "%d", &parede_1);
+                t_counter++; 
+            }
+            if (t_counter == 1) {
+                fscanf(fp, "%d", &parede_2);
+                t_counter++;
+            }
+            if (t_counter == 2) {
+                fscanf(fp, "%d", &parede_v);
+                t_counter = 0;
+                printf("%d %d %d\n", parede_1, parede_2, parede_v);
+            }
+        } else {
+            fscanf(fp, "%s", temp_string);
+            printf("%s\n", temp_string);
+        }
+    }
+
+
+
+
+    /*
+    cont = 0;
 
     for (int i = 0; i < 10; i++) {  // i indica qual é o numero da "palavra" em que vou
 
@@ -60,94 +97,10 @@ void main () {
             }
         }
         memset(temp_string, 0, sizeof(temp_string));
+        
 
         // Resolver o problema para ler as linhas das paredes e o seu valor
-
-
-
-
-
-
-
-        /*
-        // Contar o numero de algarismos do numero 
-        
-        while  (1) {
-            ch = fgetc(fp);
-
-            if (ch != ' ' && ch != '\n') {
-                cont_n++;
-            } else {
-                break;
-            }    
-        }
-        t = -cont_n - 1;
-
-        fseek(fp, t, SEEK_CUR);
-
-        // Voltar atras n espaços e carrega los para malloc
-
-        // Passar o numero para uma string
-
-        for (int j = 0; j < cont_n; j++) {
-            ch = fgetc(fp);
-
-            temp_string[j] = ch;    
-        }
-
-
-        switch (i) {
-            case 0:
-                tab_c = atoi(temp_string);
-                break;
-            case 1:
-                tab_l = atoi(temp_string);
-                break; 
-            default:
-                break;
-        }    
-
-        
-
-
-        cont_n = 0;~
-        */
-
-
-        /*
-        for (int j = 0; ch != ' ' && ch != '\n'; j++) {
-
-            ch = fgetc(fp);
-            
-            //fseek(fp, 1, SEEK_CUR);
-            ch_next = fgetc(fp);
-
-            temp_string[j] = ch;
-
-            if (ch_next == ' ' || ch_next == '\n') break;   
-
-        }
-        printf("%c %c %c %c\n", temp_string[0], temp_string[1], temp_string[2], temp_string[3]);
-
-        fseek(fp, 1, SEEK_CUR);/*
-
-
-
-        /*
-        ch = fgetc(fp);
-
-        if (ch != ' ' && ch != '\n') {
-            temp_string[cont] = ch;
-            printf("%c ", temp_string[cont]);
-            cont++;
-        } else {
-            cont = 0;
-        }*/
-
-        //printf("%c %c %c %c\n", temp_string[0], temp_string[1], temp_string[2], temp_string[3]);
-
-
-    }
+    }*/
 
     fclose(fp);
 
